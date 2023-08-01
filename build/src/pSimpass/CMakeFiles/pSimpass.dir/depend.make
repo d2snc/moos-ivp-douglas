14df0cf292618d8b568358d9b8720b185f067f6d
d2snc <doug.sonic@gmail.com>
2023-07-31 22:56:39 -0300

    Adicionado pSimPass

---
+++ build/src/pSimpass/CMakeFiles/pSimpass.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pSimpass.
+# This may be replaced when dependencies are built.
---
+++ build/src/pSimpass/CMakeFiles/pSimpass_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pSimpass_autogen"
+  "pSimpass_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pSimpass_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
